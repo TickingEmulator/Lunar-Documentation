73e54651049fca4b65b1e91ce734ecc510852b62
Tick <103855043+TickingEmulator@users.noreply.github.com>
2022-05-29 00:08:34 -0400

    Create Variables.lua
    Learn how to make a variable!

---
+++ Beginner/Variables.lua
@@ -0,0 +1,30 @@
+--[[
+    Welcome to variable section! Today we'll be teaching you how variables will help you customize your code and make it shorter!
+    
+    You may see : -- in this docs those are line comments which can be made on lines of code to help you out, they will not be ran by the compiler.
+    
+--]]
+
+  local MyName = "Tick!" -- Variable 1
+  
+ --[[ 
+  How you should structure variables is : 
+  Step One: local -- Add the function local.
+  Step Two: local Dad -- Add the name.
+  Step Three: local Dad = 0 --  Add a value in this case the value is an Int!
+ --]]
+
+--[[
+  Variables can also be strings an example is Variable 1 it contains the value of a string. Varibales can also store values like booleans.
+--]] 
+
+-- Examples bellow
+local I_Want_Food = true
+local IWantWater = false
+local NonExistient = nil
+
+--[[
+  Variables can contain underscores and numbers in the name but cannot contain spaces. You cannot start a variable with a number.
+--]]
+
+print(MyName) -- Output: Tick!
