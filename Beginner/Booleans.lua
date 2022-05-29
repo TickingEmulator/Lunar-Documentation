e7f5aa056bcbc0944f9aab02f12fff5d948ca817
Tick <103855043+TickingEmulator@users.noreply.github.com>
2022-05-29 00:13:26 -0400

    Create Booleans.lua

---
+++ Beginner/Booleans.lua
@@ -0,0 +1,7 @@
+local Are_You_Hungry = false
+
+--[[ You may have seen booleans on fundamentals and variables page today we're going to explain what they do.
+     Booleans can be used on different types of instructions, whether it being telling the program something is false or something is true.
+     An example of this is whether you are hungry. If you are not hungry than you'd probably be set to false for being hungry...Otherwise true.
+     Booleans are important in programming and can help your program make descions. More about booleans later on till than!
+--]]
